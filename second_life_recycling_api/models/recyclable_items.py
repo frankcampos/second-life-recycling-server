31f849236d0557dd9fa0f4ae5f648ba8b6b6227b
Jesse Ramirez <gedeon.ram.dev@gmail.com>
2024-07-15 19:39:37 -0500

    updated readme

--- second_life_recycling_api/models/recyclable_items.py
+++ second_life_recycling_api/models/recyclable_items.py
@@ -8,7 +8,7 @@ class Recyclable_Items(models.Model):
     vendor = models.ForeignKey(Vendors, on_delete=models.CASCADE, db_column='vendor_id', null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image_url = models.CharField(max_length=100)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     description = models.CharField(max_length=100)
     category = models.ForeignKey(Categories, on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
--- second_life_recycling_api/views/recyclable_items.py
+++ second_life_recycling_api/views/recyclable_items.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponseServerError
+from django.utils import timezone
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from django.db import models
 from datetime import datetime
-from second_life_recycling_api.models import Recyclable_Items, User
+from second_life_recycling_api.models import Recyclable_Items, User, Vendors, Categories
 from second_life_recycling_api.views.auth import check_user 
 
 class RecyclableItemsSerializer(serializers.ModelSerializer):
@@ -36,32 +37,47 @@ class RecyclableItems(ViewSet):
             Response -- JSON serialized list of recyclable items types
         """
         recyclable_items = Recyclable_Items.objects.all()
+        vendor_id = request.query_params.get("type", None)
+        if vendor_id is not None:
+            games = games.filter(vendor_id=vendor_id)
+        category_id = request.query_params.get("type", None)
+        if category_id is not None:
+            games = games.filter(vendor_id=vendor_id)    
         serializer = RecyclableItemsSerializer(recyclable_items, many=True)
         return Response(serializer.data)
         
     def create(self, request):
-        user_id = check_user(request.data.get("id", None))
-        user, created = User.objects.get_or_create(id=user_id) 
+        user_id = request.data.get("user_id", None)
+        vendor = Vendors.objects.get(pk=request.data["vendor_id"])
+        category = Categories.objects.get(pk=request.data["category_id"])
+        user, created = User.objects.get_or_create(id=user_id)
         recyclable_item = Recyclable_Items.objects.create(
             item_name=request.data["item_name"],
-            vendor=request.data["vendor"],
+            vendor=vendor,
             price=request.data["price"],
             image_url=request.data["image_url"],
-            category=request.data["category"],
-            userId=user
+            user=user,
+            description=request.data["description"],
+            category=category,
+            created_at=timezone.now(),
+            updated_at=timezone.now(),
         )
         serializer = RecyclableItemsSerializer(recyclable_item)
         return Response(serializer.data)
      
     def update(self, request, pk):
+        """Handle PUT requests for a item"""
         recyclable_item = Recyclable_Items.objects.get(pk=pk)
         recyclable_item.item_name = request.data["item_name"]
-        recyclable_item.vendor = request.data["vendor"]
+        vendors = Vendors.objects.get(pk=request.data.get("vendors"))
+        recyclable_item.vendor = vendors
         recyclable_item.price = request.data["price"]
         recyclable_item.image_url = request.data["image_url"]
-        recyclable_item.user_id = request.data["user_id"]
+        user_id = User.objects.get(pk=request.data.get("user_id"))
+        recyclable_item.user_id = user_id
         recyclable_item.description = request.data["description"]
-        recyclable_item.category = request.data["category"]
+        category = Categories.objects.get(pk=request.data.get("category"))
+        recyclable_item.category = category
         recyclable_item.created_at = request.data["created_at"]
         recyclable_item.updated_at = request.data["updated_at"]
         recyclable_item.save()
