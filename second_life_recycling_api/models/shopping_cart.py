52cf4dcc3a6782255f5d2ce83f2ac18b51b462c3
frank campos <frankcampos85@gmail.com>
2024-07-23 22:01:36 -0500

    update: Remove duplicated field, total from Shopping Cart model

--- second_life_recycling_api/models/shopping_cart.py
+++ second_life_recycling_api/models/shopping_cart.py
@@ -4,7 +4,6 @@ from .recyclable_items import Recyclable_Items
 class Shopping_Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.BooleanField(default=True)
-    total = models.DecimalField(default=0, max_digits=17, decimal_places=2)
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
 
